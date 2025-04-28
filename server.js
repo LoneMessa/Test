d2adfa0ed1b3db80e9b3be6ef63c6f266f75dca8
LoneMessa <jwhwjen@mail.ru>
2025-04-28 20:24:11 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
+
+let clients = [];
+
+server.on('connection', (socket) => {
+    clients.push(socket);
+    console.log('New client connected');
+
+    socket.on('message', (message) => {
+        console.log(`Received: ${message}`);
+        // Отправляем сообщение всем клиентам
+        clients.forEach(client => {
+            if (client !== socket && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    socket.on('close', () => {
+        clients = clients.filter(client => client !== socket);
+        console.log('Client disconnected');
+    });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
